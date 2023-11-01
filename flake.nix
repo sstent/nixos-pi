4b6c0d8b80ac8247620747860747c35f5e2d440b
sstent <stuart.stent@gmail.com>
2023-11-01 13:55:18 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,46 @@
+  GNU nano 7.2                                                  flake.nix                                                            /**
+  nix build .#rpi
+ */
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, nixos-generators, ... }: {
+    packages.aarch64-linux = {
+      rpi = nixos-generators.nixosGenerate {
+        system = "aarch64-linux";
+        modules = [
+          # you can include your own nixos configuration here, i.e.
+          # ./configuration.nix
+          {
+            config = {
+              boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+              networking.hostName = "Odroid";
+              system = {
+                stateVersion = "22.11";
+
+                # Disable zstd compression
+                # build.sdImage.compressImage = false;
+              };
+              users.users.root = {
+                openssh.authorizedKeys.keys = [
+                  "ssh-rsa ... "
+                ];
+              };
+            };
+          }
+        ];
+        format = "sd-aarch64";
+      };
+      # vbox = nixos-generators.nixosGenerate {
+      #   system = "aarch64-linux";
+      #   format = "virtualbox";
+      # };
+    };
+  };
+}
