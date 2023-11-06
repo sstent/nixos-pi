96cd5727f1d1626a6034d1e50bb1a9d861a54fe0
sstent <stuart.stent@gmail.com>
2023-11-06 11:42:56 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,30 @@
-/**
-  nix build .#odroid
-  nix build --option system aarch64-linux --option sandbox false .#odroid
- */
 {
   inputs = {
-    #nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }: let
+
+  outputs = { nixpkgs, ... }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
     };
     lib = nixpkgs.lib;
 
-in rec {
-nixosConfigurations.rpi2 = lib.nixosSystem {
+  
+
+  in rec {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rsync
+        zstd
+      ];
+    };
+
+    nixosConfigurations.rpi2 = lib.nixosSystem {
       system = "armv7l-linux";
 
       modules = [
@@ -40,61 +45,31 @@ nixosConfigurations.rpi2 = lib.nixosSystem {
       ];
     };
 
+    nixosConfigurations.rpi4 = lib.nixosSystem {
+      system = "aarch64-linux";
 
-    packages.armv7l-linux = {
-      odroid7 = nixos-generators.nixosGenerate {
-        system = "armv7l-linux";
-        modules = [
-          {
-            config = {
-              boot.binfmt.emulatedSystems = [ "armv7l-linux" ];
+      modules = [
+        {
+          imports = [
+            # https://nixos.wiki/wiki/NixOS_on_ARM#Build_your_own_image
+            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+          ];
 
-              networking.hostName = "odroid7";
-              system = {
-                stateVersion = "22.11";
+          # do not compress to zst
+          sdImage.compressImage = false;
 
-                # Disable zstd compression
-                #build.sdImage.compressImage = false;
-              };
-              # users.users.root = {
-              #   openssh.authorizedKeys.keys = [
-              #     "ssh-rsa ... "
-              #   ];
-
-              # };
-            };
-          }
-        ];
-        format = "sd-aarch64-installer";
-      };
+          services.openssh = {
+            enable = true;
+            settings.PermitRootLogin = "yes";
+          };
+          users.extraUsers.root.initialPassword = lib.mkForce "test123";
+        }
+      ];
     };
-    
-    
-    packages.aarch64-linux = {
-      odroid = nixos-generators.nixosGenerate {
-        system = "aarch64-linux";
-        modules = [
-          {
-            config = {
-              boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-              networking.hostName = "odroid";
-              system = {
-                stateVersion = "22.11";
-
-                # Disable zstd compression
-                #build.sdImage.compressImage = false;
-              };
-              # users.users.root = {
-              #   openssh.authorizedKeys.keys = [
-              #     "ssh-rsa ... "
-              #   ];
-              # };
-            };
-          }
-        ];
-        format = "sd-aarch64";
-      };
+    images = {
+      rpi2 = nixosConfigurations.rpi2.config.system.build.sdImage;
+      rpi4 = nixosConfigurations.rpi4.config.system.build.sdImage;
     };
   };
 }
